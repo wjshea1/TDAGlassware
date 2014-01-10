1dc51fc964ceb8d2767a0c778d49114ac1e8c58c
William Shea <wjshea@mac.com>
2014-01-10 11:42:57 -0500

    update the template

--- src/TDAGlassware.java
+++ src/TDAGlassware.java
@@ -1,4 +1,21 @@
+import javax.servlet.ServletContext;
+
 
 public class TDAGlassware {
 
+	public static void insertAndSaveSimpleHtmlTimelineItem( ServletContext ctx, String userId){
+		
+	}
+// Since we are updating pretty frequently it is better to the update the page then insert new ones 
+// constantly ;-) 
+	public static boolean setTDAGlasswarePageId( String userId){
+		return false;
+	}
+	
+	public static String getTDAGlasswareLastPageId( String userId){
+		
+		return null;
+		
+	}
+	
 }
--- src/com/tda/glassware/CronServlet.java
+++ src/com/tda/glassware/CronServlet.java
@@ -1,7 +1,24 @@
 package com.tda.glassware;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 public class CronServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+		
+		// Get MirrorUtils
+		MirrorUtils.getMirror(req);
+		
+		
+		// Create Pages
+		
+		
+	}
 
 }
